f9e0c88799512c359bdf5fcfa835e2cd93b0fe29
Euphyllia <bierque.euphyllia@gmail.com>
2026-06-23 01:26:24 +0200

    Add support QuickShop-Hikari (#153)
    This commit:
    - checks for permissions to create a shop on a player's island
    - removes a shop if the player leaves the island
    - removes shops if the island is deleted
    - verifies that the island is valid before allowing purchases on it
    
    It also adds new APIs:
    - SkyblockRemoveMemberEvent, triggered when an island member leaves, is banned, or is kicked from an island
    - A new PluginHook interface to facilitate adding future support
    - LanguageProvider, allowing translations to be handled via the API instead of relying on the plugin module

--- plugin/src/main/java/fr/euphyllia/skyllia/hook/HookBootstrap.java
+++ plugin/src/main/java/fr/euphyllia/skyllia/hook/HookBootstrap.java
@@ -1,11 +1,14 @@
 package fr.euphyllia.skyllia.hook;
 
+import fr.euphyllia.skyllia.Skyllia;
+import fr.euphyllia.skyllia.api.hooks.PluginHook;
 import fr.euphyllia.skyllia.api.hooks.SchematicHook;
 import fr.euphyllia.skyllia.api.hooks.ServerHook;
 import fr.euphyllia.skyllia.hook.canvas.CanvasHook;
 import fr.euphyllia.skyllia.hook.fastasyncworldedit.FAWESchematicHook;
 import fr.euphyllia.skyllia.hook.internal.InternalSchematicHook;
 import fr.euphyllia.skyllia.hook.luminol.LuminolHook;
+import fr.euphyllia.skyllia.hook.quickshop.QuickShopHook;
 import fr.euphyllia.skyllia.hook.worldedit.WorldEditSchematicHook;
 import org.bukkit.plugin.Plugin;
 import org.slf4j.Logger;
@@ -20,26 +23,43 @@ public class HookBootstrap {
     private HookBootstrap() {
     }
 
-    public static void registerAll(Plugin skylliaPlugin) {
-        List<ServerHook> serverHooks = List.of(
-                new CanvasHook(),
-                new LuminolHook()
-        );
+    static List<ServerHook> serverHooks = List.of(
+            new CanvasHook(),
+            new LuminolHook()
+    );
+
+    static List<PluginHook> pluginHooks = List.of(
+            new QuickShopHook()
+    );
+
+    static List<SchematicHook> schematicHooks = List.of(
+        new FAWESchematicHook(),
+        new WorldEditSchematicHook(),
+        new InternalSchematicHook()
+    );
+
+    public static void registerAll() {
         for (ServerHook hook : serverHooks) {
             if (!hook.isAvailable()) continue;
-            hook.register(skylliaPlugin);
-            log.info("Registered server hook: {}", hook.name());
+            hook.register(Skyllia.getInstance());
+            log.debug("Registered server hook: {}", hook.name());
         }
 
-        List<SchematicHook> schematicHooks = List.of(
-                new FAWESchematicHook(skylliaPlugin),
-                new WorldEditSchematicHook(skylliaPlugin),
-                new InternalSchematicHook(skylliaPlugin)
-        );
         for (SchematicHook hook : schematicHooks) {
             if (!hook.isAvailable()) continue;
-            log.info("Active schematic hook: {}", hook.name());
+            log.debug("Active schematic hook: {}", hook.name());
             break;
+        }
+
+        for (PluginHook pluginHook : pluginHooks) {
+            if (!pluginHook.isAvailable()) continue;
+            pluginHook.register(Skyllia.getInstance());
+        }
+    }
+
+    public static void unregisterAll() {
+        for (PluginHook pluginHook : pluginHooks) {
+            if (pluginHook.isAvailable()) pluginHook.unregister();
         }
     }
 }
